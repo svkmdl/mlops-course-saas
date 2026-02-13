586cf4e4c4a884e5a28e6e664b0856d48b968d51
svkmdl <souvikmondal15@gmail.com>
2026-02-13 01:04:28 +0100

    feat : update /api route with auth

--- api/index.py
+++ api/index.py
@@ -1,21 +1,34 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, Depends
 from fastapi.responses import StreamingResponse
+from fastapi_clerk_auth import ClerkConfig, ClerkHTTPBearer, HTTPAuthorizationCredentials
 from openai import OpenAI, OpenAIError
 import os
 
 app = FastAPI()
 
+clerk_config = ClerkConfig(jwks_url =os.getenv("CLERK_JWKS_URL"))
+clerk_guard = ClerkHTTPBearer(clerk_config)
+
 @app.get("/api", response_class=StreamingResponse)
-def idea():
-    api_key = os.environ.get("OPENAI_API_KEY")
-    if not api_key:
-        raise HTTPException(status_code=500, detail="OPENAI_API_KEY not set in environment")
+def idea(creds: HTTPAuthorizationCredentials = Depends(clerk_guard)):
+
+    user_id = creds.decoded["sub"] # User ID from JWT - for future use
+    # We now know which user is making the request
+    # How to use this info :
+    # - track usage per user
+    # - store generated ideas in db
+    # - apply user specific limits or customization
+
     client = OpenAI(
         # increase default timeout to 15 minutes (from 10 minutes) for service_tier="flex" calls to be on the safe side
-        # timeout=900.0,
-        api_key=api_key
+        # timeout=900.0
     )
-    prompt = [{"role": "user", "content": "Reply with a new business idea for AI Agents, formatted with headings, sub-headings and bullet points"}]
+    prompt = [
+        {
+            "role": "user",
+            "content": "Reply with a new business idea for AI Agents, formatted with headings, sub-headings and bullet points"
+        }
+    ]
     stream = client.chat.completions.create(model="gpt-5-nano", messages=prompt, stream=True) # service_tier="flex"
 
     def event_stream():
