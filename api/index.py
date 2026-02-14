1a168dad5178ab89177c51625b5b8905b3da4a2a
svkmdl <souvikmondal15@gmail.com>
2026-02-14 22:48:45 +0100

    chore : modified fastapi method, introduced pydantic model and changed prompt to change scope to a healthcare app

--- api/index.py
+++ api/index.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI, Depends
 from fastapi.responses import StreamingResponse
 from fastapi_clerk_auth import ClerkConfig, ClerkHTTPBearer, HTTPAuthorizationCredentials
-from openai import OpenAI, OpenAIError
+from openai import OpenAI
+from openai.types.chat import ChatCompletionMessageParam
+from pydantic import BaseModel
 import os
 
 app = FastAPI()
@@ -9,8 +11,33 @@ app = FastAPI()
 clerk_config = ClerkConfig(jwks_url =os.getenv("CLERK_JWKS_URL"))
 clerk_guard = ClerkHTTPBearer(clerk_config)
 
-@app.get("/api", response_class=StreamingResponse)
-def idea(creds: HTTPAuthorizationCredentials = Depends(clerk_guard)):
+class Visit(BaseModel):
+    patient_name: str
+    date_of_visit: str
+    notes: str
+
+system_prompt = """
+You are provided with notes written by a doctor from a patient's visit.
+Your job is to summarize the visit for the doctor and provide an email.
+Reply with exactly three sections with the headings:
+### Summary of visit for the doctor's records
+### Next steps for the doctor
+### Draft of email to patient in patient-friendly language
+"""
+# improvement idea : respond with json instead of markdown with the headings above
+
+def user_prompt_for_visit(visit: Visit) -> str :
+    return f"""Create the summary, next steps and draft email for:
+Patient name: {visit.patient_name}
+Date of visit: {visit.date_of_visit}
+Notes: 
+{visit.notes}"""
+
+@app.post("/api", response_class=StreamingResponse)
+def consultation_summary(
+        visit: Visit,
+        creds: HTTPAuthorizationCredentials = Depends(clerk_guard)
+):
 
     user_id = creds.decoded["sub"] # User ID from JWT - for future use
     # We now know which user is making the request
@@ -23,13 +50,18 @@ def idea(creds: HTTPAuthorizationCredentials = Depends(clerk_guard)):
         # increase default timeout to 15 minutes (from 10 minutes) for service_tier="flex" calls to be on the safe side
         # timeout=900.0
     )
+
+    user_prompt = user_prompt_for_visit(visit)
     prompt = [
-        {
-            "role": "user",
-            "content": "Reply with a new business idea for AI Agents, formatted with headings, sub-headings and bullet points"
-        }
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": user_prompt}
     ]
-    stream = client.chat.completions.create(model="gpt-5-nano", messages=prompt, stream=True) # service_tier="flex"
+    stream = client.chat.completions.create(
+        model="gpt-5-nano",
+        messages=prompt,
+        stream=True
+        # service_tier="flex"
+    )
 
     def event_stream():
         for chunk in stream:
