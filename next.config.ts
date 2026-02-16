1f12aba6b6b2f6708d535826fb9cf23c90fde986
svkmdl <souvikmondal15@gmail.com>
2026-02-16 22:25:16 +0100

    chore : converted to static export

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: true,
+  output: 'export', // This exports static HTML/JS files
+  images: {
+  unoptimized: true // Required for static export
+  }
 };
 
 export default nextConfig;
